9f7e366e6037fb9d7edddba6fa9298579b7e5f5d
coder cqq <2141677845@qq.com>
2021-11-14 14:49:32 +0800

    feat(*): link

---
+++ src/MyUi/components/link/index.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  name: "ElLink",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+      validate: (val) =>
+        ["default", "primary", "success", "warning", "danger", "info"].includes(
+          val
+        ),
+    },
+    underline: {
+      type: Boolean,
+      default: true
+    },
+    disabled: Boolean,
+    href: String,
+    icon: String,
+  },
+  render(h) {
+    const { type, underline, disabled, href, icon, $slots } = this;
+    const click = (e) => {
+      if (!disabled) {
+        this.$emit("click", e);
+      }
+    };
+    const classList = [
+      `el-link`,
+      `el-link--${type}`,
+      {
+        'is-underline': underline && !disabled,
+        'is-disabled': disabled,
+      },
+    ];
+    return (
+      <a
+        href={disabled ? null :href}
+        disabled={disabled}
+        class={classList}
+        onClick={click}
+      >
+        {icon && <i class={icon}></i>}
+        {$slots.default}
+      </a>
+    );
+  },
+};
+</script>
+
+<style>
+</style>
--- src/MyUi/index.js
+++ src/MyUi/index.js
@@ -4,9 +4,10 @@ import ElButton from '@/MyUi/components/button';
 import ElButtonGroup from '@/MyUi/components/button/ButtonGroup';
 import ElRow from '@/MyUi/components/row';
 import ElCol from '@/MyUi/components/col';
+import ElLink from '@/MyUi/components/link';
 
 Vue.component(ElButton.name, ElButton);
 Vue.component(ElButtonGroup.name, ElButtonGroup);
 Vue.component(ElRow.name, ElRow);
-Vue.component(ElCol.name, ElCol);
+Vue.component(ElLink.name, ElLink);
 
